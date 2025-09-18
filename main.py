8bf72371038a7872832f50b893b657bfc67347c2
Caden Epp <ccepp2412@gmail.com>
2025-09-18 09:58:53 -0500

    finished code

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request, flash, redirect, url_for, ses
 from BudgetManager import BudgetManager
 from classes.ExpenseEntry import ExpenseEntry
 from classes.IncomeEntry import IncomeEntry
+from custom_filter import format_price
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
+app.jinja_env.filters['format_price'] = format_price
 
 agent = BudgetManager()
 
@@ -18,10 +20,10 @@ def summary():
 
     if request.method == 'POST':
 
-        income_entry = float(request.form.get('income', 0) or 0)
-        expense_entry = float(request.form.get('expense', 0) or 0)
-        description_i = request.form.get('descriptionI', "No description added") or "No description added"
-        description_e = request.form.get('descriptionE', "No description added") or "No description added"
+        income_entry = float(request.form.get('income', 0))
+        expense_entry = float(request.form.get('expense', 0))
+        description_i = request.form.get('descriptionI', "No description added")
+        description_e = request.form.get('descriptionE', "No description added")
 
         if income_entry > 0:
             income_obj = IncomeEntry(income_entry, description_i)
@@ -33,13 +35,21 @@ def summary():
             agent.add_expense(expense_obj)
             session['expense'] = expense_obj.get_amount()
 
-    session['agent_tot_inc'] = agent.get_total_income()
-    session['agent_tot_exp'] = agent.get_total_expense()
-    session['agent_net_tot'] = agent.get_net_total()
+        session['agent_tot_inc'] = agent.get_total_income()
+        session['agent_tot_exp'] = agent.get_total_expense()
+        session['agent_net_tot'] = agent.get_net_total()
+
+
     incomes_list = agent.incomes
     expenses_list = agent.expenses
 
     return render_template('summary.html', incomes_list=incomes_list, expenses_list=expenses_list)
+
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 @app.errorhandler(404)
